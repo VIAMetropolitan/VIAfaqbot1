421102a1e9172756fb2e8c1ab6abe8aa0a1bf256
VIAMetro <38225614+VIAMetro@users.noreply.github.com>
2018-04-10 10:04:08 -0500

    Init
    Init

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=viafaqbot1-Web-Deploy.pubxml -p:Password=nZeNRu8eCtYa81JnWW7BELds8NXWwkrGoneN8SXg206h4TMKcST5SaWvQzQP
+
